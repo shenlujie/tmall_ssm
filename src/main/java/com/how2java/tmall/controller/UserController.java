49b655bcc5da0b50fd33f8d74f5ab0c02e1f9f39
SLJ <m18875764622@163.com>
2018-04-28 21:55:03 +0800

    后台管理-用户管理-查询 用户管理只提供查询功能

---
+++ src/main/java/com/how2java/tmall/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.how2java.tmall.controller;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.how2java.tmall.pojo.User;
+import com.how2java.tmall.service.UserService;
+import com.how2java.tmall.util.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("admin_user_list")
+    public String list(Model model, Page page){
+        List<User> list = userService.list();
+        PageHelper.offsetPage(page.getStart(),page.getCount());
+        int total = (int) new PageInfo<>(list).getTotal();
+        page.setTotal(total);
+        model.addAttribute("user",list);
+        return "admin/listUser";
+
+    }
+}
---
+++ src/main/java/com/how2java/tmall/service/UserService.java
@@ -0,0 +1,9 @@
+package com.how2java.tmall.service;
+
+import com.how2java.tmall.pojo.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> list();
+}
